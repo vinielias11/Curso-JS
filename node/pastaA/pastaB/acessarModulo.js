e43979cfd8d57bcbb68c0d894dd36f16591dfcf6
Vinicius Elias <viniciuselias79@gmail.com>
2021-07-09 23:03:02 -0300

    ultimo estudo dia 9

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -4,8 +4,11 @@ console.log(moduloA.ola)
 const saudacao = require('saudacao')
 console.log(saudacao.ola)
 
-const http = require('http')
+const c = require('./pastaC')
+console.log(c.ola2)
+
+/* const http = require('http')
 http.createServer((req, res) => {
     res.write('Bom dia!')
     res.end
-}).listen(8080)
+}).listen(8080) */
